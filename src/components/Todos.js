d77b0661d6d87d764eb154b57275bb2b037e330f
Tillmann Weimer <tillmann1970@gmail.com>
2020-11-17 10:53:50 +0100

    added PropTypes and delete todo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,11 @@
 import React from "react"
+import PropTypes from "prop-types"
 import TodoItem from "./TodoItem"
-//import PropTypes from "prop-types"
 
 class Todos extends React.Component {
   render() {
     const todoElements = this.props.todos.map(todo => {
-      return <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} />
+      return <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
     })
 
     return todoElements
@@ -13,8 +13,8 @@ class Todos extends React.Component {
 }
 
 //propTypes
-// Todos.PropTypes = {
-//   todos: PropTypes.array.isRequired
-// }
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+}
 
 export default Todos

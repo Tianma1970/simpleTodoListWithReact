8b4c3f4720d87ca6368d2f8a2dbc1c37057d9b78
Tillmann Weimer <tillmann1970@gmail.com>
2020-11-17 15:23:07 +0100

    created about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the Todo list app v1.0.0. It's a part of the React crash course</p>
+    </React.Fragment>
+  )
+}
+
+export default About

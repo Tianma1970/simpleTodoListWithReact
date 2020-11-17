112abd77c45121d9690e04ee7f2db4d2f2e9e0f9
Tillmann Weimer <tillmann1970@gmail.com>
2020-11-17 10:50:39 +0100

    created variable for Button styles

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -13,19 +13,31 @@ class TodoItem extends React.Component {
 
   render() {
     const { id, title } = this.props.todo
+    const btnStyle = {
+      background: "#ff0000",
+      color: "#fff",
+      border: "none",
+      padding: "5px 10px",
+      borderRadius: "50px",
+      cursor: "pointer",
+      float: "right"
+    }
     return (
       <div style={this.getStyle()}>
         <p>
           <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />
           {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            x
+          </button>
         </p>
       </div>
     )
   }
 }
 //propTypes
-// TodoItem.PropTypes = {
-//   todo: PropTypes.object.isRequired
-// }
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+}
 
 export default TodoItem
